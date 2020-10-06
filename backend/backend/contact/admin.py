2c2499bfa50452a05b49f7e8ab1f76d81e7f3413
Danny Jeng <jeng.danny@gmail.com>
2020-10-06 19:20:07 -0400

    Removed forms because the regular admin form is enough.

--- backend/backend/contact/admin.py
+++ backend/backend/contact/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import Contact
-from .forms import ContactForm
-
-#class ContactAdmin(admin.ModelAdmin):
-#    form = ContactForm
-#
-#    fields = ['name', 'email', 'message']
 
 # Register your models here.
-admin.site.register(Contact)#, ContactAdmin)
+admin.site.register(Contact)
