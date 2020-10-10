76d895cd8f7d6b3a7aebbebe41a4c7f5a79062e1
Danny Jeng <jeng.danny@gmail.com>
2020-10-09 22:32:00 -0400

    Removed backend.

--- backend/backend/contact/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import ContactSerializer
-from .models import Contact
-
-# Create your views here.
-class ContactViewSet(viewsets.ModelViewSet):
-    serializer_class = ContactSerializer
-    queryset = Contact.objects.all()
